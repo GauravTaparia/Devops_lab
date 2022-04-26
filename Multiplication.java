7b8890c5b089912f923a88f3a5c5789f403e50a5
GauravTaparia <62346385+GauravTaparia@users.noreply.github.com>
2022-04-26 17:46:58 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+public class Multiplication {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 * num2;
+
+      System.out.println("Multiplication of these numbers: "+sum);
+   }
+}
