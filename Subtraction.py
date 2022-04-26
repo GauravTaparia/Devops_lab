0fa1f8a709dc29b081011ae4cc17d3091223585e
GauravTaparia <62346385+GauravTaparia@users.noreply.github.com>
2022-04-26 18:09:32 +0530

    Create Subtraction.py

---
+++ Subtraction.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num2 - num 1
+
+# Display the sum
+print('The subtraction of {1} and {0} is {2}'.format(num1, num2, sum))
