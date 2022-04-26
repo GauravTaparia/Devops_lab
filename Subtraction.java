cbfb6735a747ac738af04ed1fc0a65b8c5ab96cf
GauravTaparia <62346385+GauravTaparia@users.noreply.github.com>
2022-04-26 17:50:44 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+public class Subtraction {
+
+   public static void main(String[] args) {
+        
+      int num1 = 15, num2 = 5, sum;
+      sum = num1 - num2;
+
+      System.out.println("Subtraction of these numbers: "+sum);
+   }
+}
